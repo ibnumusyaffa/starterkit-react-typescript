f2635eee8690a2d5b44502f9bd3a3cb15827c952
Ibnu Musyaffa <ibnu.musyaffa@gmail.com>
2023-06-03 17:16:17 +0700

    tooltip.ts

---
+++ components/tooltip/Tooltip.tsx
@@ -0,0 +1,152 @@
+import * as TooltipPrimitive from '@radix-ui/react-tooltip'
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useRef,
+  useEffect,
+} from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
+import { useControllableState } from '@/hooks'
+import cx from 'clsx'
+
+type ProviderParams = {
+  open?: boolean
+  setOpen?: (value: boolean) => void
+}
+
+const TooltipCtx = createContext<ProviderParams>({})
+
+export function TooltipTrigger(props: { children: React.ReactNode }) {
+  return (
+    <TooltipPrimitive.Trigger asChild>
+      {props.children}
+    </TooltipPrimitive.Trigger>
+  )
+}
+
+export type TooltipRootProps = TooltipPrimitive.TooltipProviderProps &
+  TooltipPrimitive.TooltipProps
+
+export function TooltipRoot({
+  children,
+  open,
+  onOpenChange,
+  defaultOpen = false,
+  delayDuration = 700,
+  skipDelayDuration = 300,
+  disableHoverableContent,
+}: TooltipRootProps) {
+  const [_open, _setOpen] = useControllableState<boolean>({
+    value: open,
+    onChange: onOpenChange,
+    defaultValue: defaultOpen,
+  })
+  return (
+    <span>
+      <TooltipCtx.Provider value={{ open: _open, setOpen: _setOpen }}>
+        <TooltipPrimitive.Provider skipDelayDuration={skipDelayDuration}>
+          <TooltipPrimitive.Root
+            open={_open}
+            defaultOpen={_open}
+            onOpenChange={(value) => _setOpen(value)}
+            delayDuration={delayDuration}
+            disableHoverableContent={disableHoverableContent}
+          >
+            {children}
+          </TooltipPrimitive.Root>
+        </TooltipPrimitive.Provider>
+      </TooltipCtx.Provider>
+    </span>
+  )
+}
+
+export type TooltipContentProps = React.ComponentPropsWithoutRef<
+  typeof TooltipPrimitive.Content
+> & {
+  color: 'dark' | 'light'
+}
+
+export const TooltipContent = React.forwardRef<
+  React.ElementRef<typeof TooltipPrimitive.Content>,
+  TooltipContentProps
+>(
+  (
+    {
+      children,
+      color = 'light',
+      side = 'top',
+      sideOffset = 4,
+      align = 'center',
+      alignOffset = 0,
+      ...props
+    },
+    ref
+  ) => {
+    const { open } = useContext(TooltipCtx)
+
+    //workaround for radix bug
+    const [, forceRender] = useState(0)
+    const containerRef = useRef<HTMLElement | null>(null)
+    useEffect(() => {
+      containerRef.current = document.body
+      forceRender((prev) => prev + 1)
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    return (
+      <AnimatePresence>
+        {open ? (
+          <TooltipPrimitive.Portal container={containerRef.current} forceMount>
+            <TooltipPrimitive.Content
+              ref={ref}
+              forceMount
+              side={side}
+              align={align}
+              sideOffset={sideOffset}
+              alignOffset={alignOffset}
+              {...props}
+            >
+              <motion.div
+                initial={{
+                  opacity: 0,
+                }}
+                animate={{
+                  opacity: 1,
+                  transition: { duration: 0.15, ease: 'easeOut' },
+                }}
+                exit={{
+                  opacity: 0,
+                  transition: { duration: 0.1, ease: 'easeIn' },
+                }}
+                className={cx('relative rounded px-2 py-2', {
+                  'bg-gray-800 text-white': color === 'dark',
+                  'bg-white text-gray-700 shadow-md ring-1 ring-black ring-opacity-5':
+                    color === 'light',
+                })}
+              >
+                <TooltipPrimitive.Arrow asChild>
+                  <div className="relative h-[4px] w-[13px]">
+                    <div
+                      className={cx(
+                        'absolute h-2 w-2 rotate-45',
+                        ' left-0 right-0 ml-auto mr-auto',
+                        {
+                          'top-[-4px] bg-gray-800': color === 'dark',
+                          'top-[-3.5px] border-r border-b border-black border-opacity-[0.15] bg-white shadow-md':
+                            color === 'light',
+                        }
+                      )}
+                    ></div>
+                  </div>
+                </TooltipPrimitive.Arrow>
+                <span className="block text-sm">{children}</span>
+              </motion.div>
+            </TooltipPrimitive.Content>
+          </TooltipPrimitive.Portal>
+        ) : null}
+      </AnimatePresence>
+    )
+  }
+)
+TooltipContent.displayName = TooltipPrimitive.Content.displayName
--- pages/docs/tooltip.tsx
+++ pages/docs/tooltip.tsx
@@ -7,6 +7,9 @@ import {
 import { Button } from '@/components/button'
 
 function Page() {
+
+  const sides = ['top', 'right', 'bottom', 'left'] as const
+  const aligns = ['start', 'center', 'end'] as const
   return (
     <div className="space-y-5">
       <div className="space-y-2">
@@ -39,8 +42,8 @@ function Page() {
       <div className="space-y-3">
         <div className="text-xl font-semibold text-gray-700">Positioning</div>
         <div className="grid grid-cols-2 gap-5 md:w-3/4 md:grid-cols-5">
-          {['top', 'right', 'bottom', 'left'].map((side, sideIndex) => {
-            return ['start', 'center', 'end'].map((align, alignIndex) => {
+          {sides.map((side, sideIndex) => {
+            return aligns.map((align, alignIndex) => {
               return (
                 <TooltipRoot key={`${sideIndex}-${alignIndex}`}>
                   <TooltipTrigger>
