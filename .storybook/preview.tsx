f238be247923d4fd8b77ba50f24f1ef5dc722073
ibnu.musyaffa <ibnu.musyaffa@gmail.com>
2024-01-28 15:45:08 +0700

    add storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import type { Preview } from '@storybook/react'
 import theme from './theme'
+import '@/styles/globals.css'
 
 const preview: Preview = {
   globalTypes: {
