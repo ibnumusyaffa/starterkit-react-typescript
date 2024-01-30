07199594389f18aa6f2b5c815ca7ec7eea0bb35c
ibnu.musyaffa <ibnu.musyaffa@gmail.com>
2024-01-30 14:20:34 +0700

    add alert dialog

---
+++ components/alert-dialog/AlertDialog.stories.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useState } from 'react'
+import type { StoryObj } from '@storybook/react'
+
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogCloseButton,
+  AlertDialogContent,
+  AlertDialogFooter,
+} from '@/components/alert-dialog'
+import { Button } from '@/components/button'
+
+const meta = {
+  title: 'Overlay/AlertDialog',
+}
+
+export default meta
+
+// type Story = StoryObj<typeof meta>
+
+export const BasicUsage: StoryObj = {
+  render: () => {
+    const [open, setOpen] = useState(false)
+    return (
+      <React.Fragment>
+        <Button variant="default" onClick={() => setOpen(!open)}>
+          Simple
+        </Button>
+        <AlertDialog open={open} onOpenChange={(value) => setOpen(value)}>
+          <AlertDialogCloseButton></AlertDialogCloseButton>
+          <AlertDialogContent
+            title="Delete post"
+            description="Are you sure you want to delete this post ? This action cannot be undone."
+          ></AlertDialogContent>
+          <AlertDialogFooter>
+            <AlertDialogCancel>
+              <Button variant="default">Cancel</Button>
+            </AlertDialogCancel>
+            <Button color="danger" variant="solid">
+              Submit
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialog>
+      </React.Fragment>
+    )
+  },
+}
