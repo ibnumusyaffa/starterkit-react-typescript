5d47d3d7c595a165f788384fd63278935c9ac34e
ibnu.musyaffa <ibnu.musyaffa@gmail.com>
2024-03-03 19:07:26 +0700

    remove unused file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,12 @@ import React from 'react'
 import type { AppProps } from 'next/app'
 import { Inter as Font } from 'next/font/google'
 import Head from 'next/head'
-// import { I18nProvider } from '@/locales'
+import { I18nProvider } from '@/locales'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Toaster } from 'react-hot-toast'
-
-// import id from '@/locales/id'
-
+import id from '@/locales/id'
 import '@/styles/globals.css'
-
-import Layout from '@/layouts/docs'
 
 const font = Font({
   subsets: ['latin'],
@@ -34,11 +30,9 @@ function MyApp({ Component, pageProps }: AppProps) {
       <QueryClientProvider client={queryClient}>
         <Toaster gutter={12} />
 
-        {/* <I18nProvider locale={pageProps.locale} fallbackLocale={id}> */}
-        <Layout>
+        <I18nProvider locale={pageProps.locale} fallbackLocale={id}>
           <Component {...pageProps} />
-        </Layout>
-        {/* </I18nProvider> */}
+        </I18nProvider>
 
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,5 @@
 import React from 'react'
 
-import { CircularProgress } from '@/components/circular-progress'
-
 export default function Page() {
-  const [value, setValue] = React.useState(0)
-
-  React.useEffect(() => {
-    const interval = setInterval(() => {
-      setValue((value) => (value + 1) % 101)
-    }, 100)
-
-    return () => clearInterval(interval)
-  }, [])
-  const colors = ['xs', 'sm', 'md', 'lg', 'xl'] as const
-
-  return (
-    <div className="space-y-5">
-      {colors.map((item) => {
-        return (
-          <CircularProgress
-            key={item}
-            color="primary"
-            value={value}
-            size={item}
-          ></CircularProgress>
-        )
-      })}
-    </div>
-  )
+  return <div className="space-y-5">It Works</div>
 }
